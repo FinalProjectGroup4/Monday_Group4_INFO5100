d137705968a51637e408c9ac8378dc798eafddcc
codexvala-da <vala.da@northeastern.edu>
2024-12-04 21:17:37 -0500

    initial commit

---
+++ Organ donation platform/src/models/Enterprise/Enterprise.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models.Enterprise;
+
+/**
+ *
+ * @author apple
+ */
+public abstract class Enterprise {
+    
+    private EnterpriseType enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+    
+    public enum EnterpriseType{
+        Hospital("Hospital");
+        
+        private String value;
+        
+        private EnterpriseType(String value){
+            this.value=value;
+        }
+        public String getValue() {
+            return value;
+        }
+        @Override
+        public String toString(){
+        return value;
+    }
+    }
+    
+}
