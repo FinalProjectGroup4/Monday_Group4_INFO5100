2f8827dc9ab4d41184f16e27df5afd7c1463b482
codexvala-da <vala.da@northeastern.edu>
2024-12-07 22:11:43 -0500

    support added for enterprise role management

---
+++ src/Model/Roles/EnterpriseAdminRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Roles;
+
+import Model.EcoSystem;
+import Model.Enterprises.Enterprise;
+import Model.Organization.Organization;
+import Model.UserAccount.UserAccount;
+import Model.storage.PatientDirectory;
+import UI.SystemAdmin.SystemAdminMainWorkArea;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author apple
+ */
+public class EnterpriseAdminRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system,PatientDirectory patientDirectory) {
+        return new SystemAdminMainWorkArea(userProcessContainer, system);
+    }
+    
+}
