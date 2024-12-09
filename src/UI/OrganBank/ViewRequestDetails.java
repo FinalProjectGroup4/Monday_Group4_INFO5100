02381cfc9a75f4d198f8bea43e2d3bddc2b2bf45
codexvala-da <vala.da@northeastern.edu>
2024-12-08 23:13:15 -0500

    populate fields

--- src/UI/OrganBank/ViewRequestDetails.java
+++ src/UI/OrganBank/ViewRequestDetails.java
@@ -29,6 +29,7 @@ public class ViewRequestDetails extends javax.swing.JPanel {
         this.userProcessContainer = container;
         this.organProcurement = organProcurement;
         this.enterprise = enterprise;
+        populateFields();
     }
 
     /**
@@ -47,14 +48,8 @@ public class ViewRequestDetails extends javax.swing.JPanel {
         txtOrgan = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         txtBloodGroup = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        txtDoctor = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        txtHospital = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
-        txtCity = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
-        txtCountry = new javax.swing.JTextField();
+        txtStatus = new javax.swing.JTextField();
         btnApproval = new javax.swing.JButton();
         btnReject = new javax.swing.JButton();
         btnBack = new javax.swing.JButton();
@@ -67,13 +62,7 @@ public class ViewRequestDetails extends javax.swing.JPanel {
 
         jLabel3.setText("Blood Group :");
 
-        jLabel4.setText("Doctor :");
-
-        jLabel5.setText("Hospital :");
-
-        jLabel6.setText("City :");
-
-        jLabel7.setText("Country :");
+        jLabel7.setText("Status :");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -83,22 +72,16 @@ public class ViewRequestDetails extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE)
                         .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING))
-                    .addComponent(jLabel6)
                     .addComponent(jLabel7))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(txtPatientName)
                     .addComponent(txtOrgan)
                     .addComponent(txtBloodGroup)
-                    .addComponent(txtDoctor)
-                    .addComponent(txtHospital)
-                    .addComponent(txtCity)
-                    .addComponent(txtCountry, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE))
+                    .addComponent(txtStatus, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE))
                 .addContainerGap(180, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -117,22 +100,10 @@ public class ViewRequestDetails extends javax.swing.JPanel {
                     .addComponent(jLabel3)
                     .addComponent(txtBloodGroup, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4)
-                    .addComponent(txtDoctor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(txtHospital, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(txtCity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
-                    .addComponent(txtCountry, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                    .addComponent(txtStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(128, Short.MAX_VALUE))
         );
 
         btnApproval.setText("Send for Approval");
@@ -223,17 +194,18 @@ public class ViewRequestDetails extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txtBloodGroup;
-    private javax.swing.JTextField txtCity;
-    private javax.swing.JTextField txtCountry;
-    private javax.swing.JTextField txtDoctor;
-    private javax.swing.JTextField txtHospital;
     private javax.swing.JTextField txtOrgan;
     private javax.swing.JTextField txtPatientName;
+    private javax.swing.JTextField txtStatus;
     // End of variables declaration//GEN-END:variables
+
+    private void populateFields() {
+        txtPatientName.setText(organProcurement.getOrganRequest().getPatient().getName());
+        txtOrgan.setText(organProcurement.getOrganRequest().getOrganName());
+        txtBloodGroup.setText(organProcurement.getOrganRequest().getBloodGroup());
+        txtStatus.setText(organProcurement.getStatus());
+    }
 }
